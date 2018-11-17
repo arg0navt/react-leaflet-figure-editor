8338597bdc2fed6026ef45b112d8fbed4666ce90
argonavt11 <argonavt.al@yandex.ru>
2018-11-17 13:56:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import FigureEditor from "./react-leaflet-figure-editor";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <FigureEditor />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/react-leaflet-figure-editor/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class FigureEditor extends React.Component {
+    public render() {
+        return(
+            <div>Hi!</div>
+        );
+    }
+} 
