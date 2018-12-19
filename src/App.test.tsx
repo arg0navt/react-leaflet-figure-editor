1901aa6e6a247cdb6ec3df91632e58325f58d78d
argonavt11 <argonavt.al@yandex.ru>
2018-12-19 11:12:38 +0300

    test informationAboutCirclr

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,7 @@ import FigureEditor from "./react-leaflet-figure-editor";
 import AddFigureType from "./react-leaflet-figure-editor/addFigureType";
 import ListFigures from "./react-leaflet-figure-editor/listFigures";
 import InformationAboutPolygon from "./react-leaflet-figure-editor/informationPolygon";
+import InformationAboutCircle from "./react-leaflet-figure-editor/informationCircle";
 import { Map, TileLayer } from "react-leaflet";
 import * as Adapter from "enzyme-adapter-react-16";
 import { configure, shallow } from "enzyme";
@@ -21,7 +22,6 @@ it("render default", () => {
       </Map>
     </div>
   );
-  //   const output = shallow(wrapper);
   expect(wrapper);
 });
 
@@ -34,23 +34,29 @@ it("addFigureType", () => {
       figure.type === "Point"
     ) {
       if (figure.type === "Polygon") {
-          delete figure.id
-          expect(figure).toEqual({type: "Polygon", coordinates: [[]]})
+        delete figure.id;
+        expect(figure).toEqual({ type: "Polygon", coordinates: [[]] });
       }
       if (figure.type === "Circle") {
-        delete figure.id
-        expect(figure).toEqual({type: "Circle", coordinates: [], pointRadius: [], radius: 0, properties: {radius_units: "m"}})
+        delete figure.id;
+        expect(figure).toEqual({
+          type: "Circle",
+          coordinates: [],
+          pointRadius: [],
+          radius: 0,
+          properties: { radius_units: "m" }
+        });
       }
       if (figure.type === "LineString") {
-        delete figure.id
-        expect(figure).toEqual({type: "LineString", coordinates: []})
+        delete figure.id;
+        expect(figure).toEqual({ type: "LineString", coordinates: [] });
       }
       if (figure.type === "Point") {
-        delete figure.id
-        expect(figure).toEqual({type: "Point", coordinates: []})
+        delete figure.id;
+        expect(figure).toEqual({ type: "Point", coordinates: [] });
       }
     } else {
-      expect(() => {}).toThrowError("🔥 not correct type 🔥")
+      expect(() => {}).toThrowError("🔥 not correct type 🔥");
     }
   };
   const wrapper = shallow(<AddFigureType addFigure={returnFigure} />);
@@ -62,32 +68,104 @@ it("addFigureType", () => {
 
 it("listFigures", () => {
   const changeActiveFigure = id => {
-    wrapper.setProps({activeFigureID: id})
-    wrapper.update()
-  }
-  const wrapper = shallow(<ListFigures
-    figures={[]}
-    activeFigureID={null}
-    changeActiveFigure={changeActiveFigure}
-  />);
-  wrapper.setProps({figures: [{type: "Polygon", coordinates: [[]], id: "1"}]})
-  wrapper.setProps({figures: [{type: "Polygon", coordinates: [[]], id: "1"}, {type: "Polygon", coordinates: [[]], id: "2"}]})
-  expect(wrapper.find('.list-figures').childAt(0).props().params).toEqual({ type: 'Polygon', coordinates: [ [] ], id: '1' })
-  expect(wrapper.find('.list-figures').childAt(1).props().params).toEqual({ type: 'Polygon', coordinates: [ [] ], id: '2' })
-  wrapper.setProps({activeFigureID: "1"})
-  expect(wrapper.find('.list-figures').childAt(0).html()).toEqual('<div class="figure-item"><p>Polygon</p><div class="figure-item-active"></div></div>')
-})
+    wrapper.setProps({ activeFigureID: id });
+    wrapper.update();
+  };
+  const wrapper = shallow(
+    <ListFigures
+      figures={[]}
+      activeFigureID={null}
+      changeActiveFigure={changeActiveFigure}
+    />
+  );
+  wrapper.setProps({
+    figures: [{ type: "Polygon", coordinates: [[]], id: "1" }]
+  });
+  wrapper.setProps({
+    figures: [
+      { type: "Polygon", coordinates: [[]], id: "1" },
+      { type: "Polygon", coordinates: [[]], id: "2" }
+    ]
+  });
+  expect(
+    wrapper
+      .find(".list-figures")
+      .childAt(0)
+      .props().params
+  ).toEqual({ type: "Polygon", coordinates: [[]], id: "1" });
+  expect(
+    wrapper
+      .find(".list-figures")
+      .childAt(1)
+      .props().params
+  ).toEqual({ type: "Polygon", coordinates: [[]], id: "2" });
+  wrapper.setProps({ activeFigureID: "1" });
+  expect(
+    wrapper
+      .find(".list-figures")
+      .childAt(0)
+      .html()
+  ).toEqual(
+    '<div class="figure-item"><p>Polygon</p><div class="figure-item-active"></div></div>'
+  );
+});
 
 it("inforationPolygon", () => {
-  const callbackDeletePoint = index => console.log(index)
-  const callbackDeleteFigure = id => console.log(id)
-  const activeFigure = {type: "Polygon", id: "1", coordinates: [[]]}
-  const wrapper = shallow(<InformationAboutPolygon
-    deletePolygonPoint={callbackDeletePoint}
-    deleteFigure={callbackDeleteFigure}
-    figure={activeFigure}
-  />)
-  expect(wrapper.html()).toEqual('<div class="figure-info"><p>Points</p><div class="figure-info__points"></div><button>Delete figure</button></div>')
-  wrapper.setProps({figure: {type: "Polygon", id: "1", coordinates: [[[47.445745, 40.272891666666666], [47.445745, 40.272891666666666]]]}})
-  expect(wrapper.html()).toEqual('<div class="figure-info"><p>Points</p><div class="figure-info__points"><p>47.445745, 40.272891666666666</p><p>47.445745, 40.272891666666666</p></div><button>Delete figure</button></div>')
-})
+  const callbackDeletePoint = index => console.log(index);
+  const callbackDeleteFigure = id => console.log(id);
+  const activeFigure = { type: "Polygon", id: "1", coordinates: [[]] };
+  const wrapper = shallow(
+    <InformationAboutPolygon
+      deletePolygonPoint={callbackDeletePoint}
+      deleteFigure={callbackDeleteFigure}
+      figure={activeFigure}
+    />
+  );
+  expect(wrapper.html()).toEqual(
+    '<div class="figure-info"><p>Points</p><div class="figure-info__points"></div><button>Delete figure</button></div>'
+  );
+  wrapper.setProps({
+    figure: {
+      type: "Polygon",
+      id: "1",
+      coordinates: [
+        [[47.445745, 40.272891666666666], [47.445745, 40.272891666666666]]
+      ]
+    }
+  });
+  expect(wrapper.html()).toEqual(
+    '<div class="figure-info"><p>Points</p><div class="figure-info__points"><p>47.445745, 40.272891666666666</p><p>47.445745, 40.272891666666666</p></div><button>Delete figure</button></div>'
+  );
+});
+
+it("inforationCircle", () => {
+  const callbackDeleteFigure = id => console.log(id);
+  const activeFigure = {
+    type: "Circle",
+    id: "1",
+    coordinates: [],
+    pointRadius: [],
+    radius: 0,
+    properties: { radius_units: "m" }
+  };
+  const wrapper = shallow(
+    <InformationAboutCircle
+      deleteFigure={callbackDeleteFigure}
+      figure={activeFigure}
+    />
+  );
+  expect(wrapper.html()).toEqual(
+    '<div class="figure-info"><p style="margin-bottom:0">Don&#x27;t have data</p><button>Delete figure</button></div>'
+  );
+  wrapper.setProps({
+    figure: {
+      type: "Circle",
+      id: "1",
+      coordinates: [47.445745, 40.272891666666666],
+      pointRadius: [47.445745, 40.272891666666666],
+      radius: 200,
+      properties: { radius_units: "m" }
+    }
+  });
+  expect(wrapper.html()).toEqual('<div class="figure-info"><p>Center: <span>47.445745, 40.272891666666666</span></p><p>Radius: <span>200.00</span></p><button>Delete figure</button></div>')
+});
